a873fd756634e52711d9f53c82b781d393d8da86
Woogie <bgleam0183@kakao.com>
2025-12-29 01:17:17 +0900

    Initial Commit

---
+++ Something Todo/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Something Todo
+//
+//  Created by 구성욱 on 12/29/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Something Todo/Something_TodoApp.swift
@@ -0,0 +1,17 @@
+//
+//  Something_TodoApp.swift
+//  Something Todo
+//
+//  Created by 구성욱 on 12/29/25.
+//
+
+import SwiftUI
+
+@main
+struct Something_TodoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
