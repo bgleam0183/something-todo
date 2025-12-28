d8a8513179a2ab0384e57685f0f61a948bac069e
Woogie <bgleam0183@kakao.com>
2025-12-29 02:34:33 +0900

    체크박스 & 목록 출력

--- Something Todo/ContentView.swift
+++ Something Todo/ContentView.swift
@@ -7,15 +7,40 @@
 
 import SwiftUI
 
+struct TodoItems: Identifiable {
+    let id = UUID()
+    var title: String
+    var isDone: Bool
+    var dueDate: Date?
+}
+
 struct ContentView: View {
+    @State var todoITems: [TodoItems] = [
+        TodoItems(title: "Learn SwiftUI", isDone: false, dueDate: nil),
+        TodoItems(title: "Build a SwiftUI app", isDone: false, dueDate: nil),
+    ]
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+//        VStack {
+//            Image(systemName: "globe")
+//                .imageScale(.large)
+//                .foregroundStyle(.tint)
+//            Text("Hello, world!")
+//        }
+//        .padding()
+        HStack {
+            
+            Spacer()
         }
         .padding()
+        
+        ForEach($todoITems) { $item in
+            HStack {
+                Image(systemName: "checkmark.square.fill")
+                Toggle(item.title, isOn: $item.isDone)
+            }
+            .padding()
+        }
     }
 }
 
